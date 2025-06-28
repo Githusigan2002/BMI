504481e4186144840dd3b2d9c07a5df0ec3e7438
Githusigan2002 <jeyarupangithusigan@gmail.com>
2025-06-28 11:57:54 +0530

    Changes design & Write Logic- v2

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,51 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [height, setHeight] = useState("178");
-  const [weight, setWeight] = useState("70");
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
   const [bmi, setBmi] = useState(null);
   const [status, setStatus] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const Clear = () => {
+    setHeight("");
+    setWeight("");
+    setBmi(null);
+    setStatus("");  
+  };
+
+  const CalBMI = () => {
+
+    const isValidHeight = /^\d+$/.test(height);
+    const isValidWeight = /^\d+$/.test(weight);
+
+    if (isValidHeight && isValidWeight) {
+      const heightInM = height / 100;
+      const BMI = weight / (heightInM * heightInM);
+      setBmi(BMI.toFixed(2));
+      if (BMI < 8.5) {
+        setStatus("Under Weight");
+      }
+      else if (BMI >= 18.5 && BMI < 24.9) {
+        setStatus("Normal");
+      }
+
+      else if (BMI >= 25 && BMI < 29.9) {
+        setStatus("Over Weight");
+      }
+
+      else {
+        setStatus("Obese")
+      }
+      setErrorMsg("");
+
+    }
+    else {
+      setBmi(null);
+      setStatus("");
+      setErrorMsg("Please Enter numeric value");
+    }
+  };
 
   return (
     <>
@@ -14,19 +55,23 @@ function App() {
         <div className="box"></div>
         <div className="data">
           <h1>BMI Calculator</h1>
+          {errorMsg && <p className='error'>{errorMsg}</p>}
           <div className="input-container">
-            <label htmlFor="height" >Height</label>
-            <input type="text" value={height} name="" id="height" />
+            <label htmlFor="height" >Height (cm)</label>
+            <input onChange={(e) => setHeight(e.target.value)} type="text" value={height} name="" id="height" />
           </div>
           <div className="input-container">
-            <label htmlFor="weight">Weight</label>
-            <input type="text" value={weight} name="" id="weight" />
+            <label htmlFor="weight">Weight (Kg)</label>
+            <input onChange={(e) => setWeight(e.target.value)} type="text" value={weight} name="" id="weight" />
           </div>
-          <button className='btn'>Calculate BMI</button>
-          <div className="results">
-            <p>Your BMI is 23.2</p>
-            <p>Status : Normal</p>
-          </div>
+          <button onClick={CalBMI} className='btn btn-cal'>Calculate BMI</button>
+          <button onClick={Clear} className='btn btn-clear'>Clear</button>
+          {bmi !== null && (
+            <div className="results">
+              <p>Your BMI is {bmi}</p>
+              <p>Status : {status}</p>
+            </div>
+          )}
         </div>
       </div>
     </>
